62630f2116d71a0b27df3a7c9bc1ea3b50bc315f
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2022-05-13 17:33:14 -0300

    update

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -3,12 +3,16 @@ const { response, request } = require('express');
 
 const orderNew = (req = request, res = response) => {
 
+    console.log(res.body);
+
      res.json({
         msg: 'Endpoint Deshabilitado',
     });
 }
 
 const orderUpdate = (req, res = response) => {
+
+    console.log(res.body);
 
      res.json({
         msg: 'Endpoint Deshabilitado',
