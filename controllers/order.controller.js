ed82eb72478ea26999e4725947240eb52cd39d88
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2022-05-17 11:31:25 -0300

    asdasd

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -4,80 +4,106 @@ const finnegService = require('../services/finneg.service');
 
 const orderNew = async (req = request, res = response) => {
 
-    const { data: token } = await finnegService.auth();
+    try {
 
-    let items = [];
+        const { data: token } = await finnegService.auth();
 
-    for (let i = 0; i < req.body.line_items.length; i++) {
-        items.push({
-            vinculacionOrigen: req.body.line_items[i].product_id,
-            Precio: req.body.line_items[i].price,
-            ProductoCodigo: req.body.line_items[i].sku,
-            Descripcion: req.body.line_items[i].title,
-            Cantidad: req.body.line_items[i].quantity
+        let items = [];
+
+        for (let i = 0; i < req.body.line_items.length; i++) {
+            items.push({
+                vinculacionOrigen: req.body.line_items[i].product_id,
+                Precio: req.body.line_items[i].price,
+                ProductoCodigo: req.body.line_items[i].sku,
+                Descripcion: req.body.line_items[i].title,
+                Cantidad: req.body.line_items[i].quantity
+            });
+        }
+
+        const order = {
+            Cliente: "Prueba",
+            IdentificacionExterna: req.body.number,
+            EmpresaCodigo: "1",
+            Fecha: req.body.created_at,
+            CondicionPagoCodigo: "CE",
+            TransaccionSubtipoCodigo: "PEDVTA",
+            Items: items,
+            Descripcion: "",
+            TransaccionTipoCodigo: "OPER",
+            WorkflowCodigo: "VENTAS",
+            MonedaCodigo: "PES",
+        }
+
+        const { data } = await finnegService.orderNew(order, token);
+
+        console.log(order, data.data);
+
+        res.json({
+            msg: 'OK',
         });
+
+    } catch (error) {
+
+        console.log(error);
+
+        res.json({
+            msg: 'Error',
+        });
+
     }
 
-    const order = {
-        Cliente: "Prueba",
-        IdentificacionExterna: req.body.number,
-        EmpresaCodigo: "1",
-        Fecha: req.body.created_at,
-        CondicionPagoCodigo: "CE",
-        TransaccionSubtipoCodigo: "PEDVTA",
-        Items: items,
-        Descripcion: "",
-        TransaccionTipoCodigo: "OPER",
-        WorkflowCodigo: "VENTAS",
-        MonedaCodigo: "PES",
-    }
-
-    const { data } = await finnegService.orderNew(order, token);
-
-    console.log(order, data);
-
-    res.json({
-        msg: 'OK',
-    });
+    
 }
 
 const orderUpdate = async (req, res = response) => {
 
-    const { data: token } = await finnegService.auth();
+    try {
 
-    let items = [];
+        const { data: token } = await finnegService.auth();
 
-    for (let i = 0; i < req.body.line_items.length; i++) {
-        items.push({
-            vinculacionOrigen: req.body.line_items[i].product_id,
-            Precio: req.body.line_items[i].price,
-            ProductoCodigo: req.body.line_items[i].sku,
-            Descripcion: req.body.line_items[i].title,
-            Cantidad: req.body.line_items[i].quantity
+        let items = [];
+
+        for (let i = 0; i < req.body.line_items.length; i++) {
+            items.push({
+                vinculacionOrigen: req.body.line_items[i].product_id,
+                Precio: req.body.line_items[i].price,
+                ProductoCodigo: req.body.line_items[i].sku,
+                Descripcion: req.body.line_items[i].title,
+                Cantidad: req.body.line_items[i].quantity
+            });
+        }
+
+        const order = {
+            Cliente: "Prueba",
+            IdentificacionExterna: req.body.number,
+            EmpresaCodigo: "1",
+            Fecha: req.body.created_at,
+            CondicionPagoCodigo: "CE",
+            TransaccionSubtipoCodigo: "PEDVTA",
+            Items: items,
+            Descripcion: "",
+            TransaccionTipoCodigo: "OPER",
+            WorkflowCodigo: "VENTAS",
+            MonedaCodigo: "PES",
+        }
+
+        const { data } = await finnegService.orderUpdate(order, token);
+
+        console.log(order, data.data);
+
+        res.json({
+            msg: 'OK',
         });
+
+    } catch (error) {
+
+        console.log(error);
+
+        res.json({
+            msg: 'Error',
+        });
+
     }
-
-    const order = {
-        Cliente: "Prueba",
-        IdentificacionExterna: req.body.number,
-        EmpresaCodigo: "1",
-        Fecha: req.body.created_at,
-        CondicionPagoCodigo: "CE",
-        TransaccionSubtipoCodigo: "PEDVTA",
-        Items: items,
-        Descripcion: "",
-        TransaccionTipoCodigo: "OPER",
-        WorkflowCodigo: "VENTAS",
-        MonedaCodigo: "PES",
-    }
-
-    const { data } = await finnegService.orderUpdate(order, token);
-
-    console.log(order, data);
-
-    res.json({
-        msg: 'OK',
-    });
 }
 
 
